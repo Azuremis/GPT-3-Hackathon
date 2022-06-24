ea168cd39e77672f57b0f80fd455608db8d8a264
Adrian <adbanachowicz@gmail.com>
2022-06-24 18:40:37 +0200

    Bot marv

---
+++ interface.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from gpt import Gpt
+from prompt import prompt_marv_qa
+
+st.header("GPT-3 application")
+
+api_key = st.text_input("OpenAI API Key:", type="password")
+
+st.header("Your personal sarcastic bot - Marv")
+
+question_for_marv = st.text_input("Question for Marv:")
+
+gpt = Gpt(api_key)
+
+if st.button("Answer"):
+    st.write(gpt.gpt_3(0.8, prompt_marv_qa(question_for_marv)))
+
+
